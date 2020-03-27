0e6663fe947f9bc2c84ea4be69be7bec362957e2
vindx <kiruha9414@gmail.com>
2020-03-27 22:42:57 +0300

    changed for heroku part 2

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(PORT, () =>
+      console.log(`App has been started at port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server ERROR!", e.message);
+    process.exit(1);
+  }
+};
+
+start();
